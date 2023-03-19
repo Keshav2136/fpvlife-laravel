4320bb6e5995ea8a769f9f9669111584e0d52e8f
Keshav Sharma <keshav.simple.123@gmail.com>
2023-03-19 15:30:28 +0530

    Create user page. Added its route.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/{user}', function ($user) {
+    return view('user',['user' => $user]);
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
